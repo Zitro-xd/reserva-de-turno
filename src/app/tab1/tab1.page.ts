4c4c17a227eb5b30fec86a48d693c2d04e7e5750
Davih_xd <elweydelpito1234@gmail.com>
2026-09-24 20:30:52 -0300

    main

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+interface Horario {
+  hora: string;
+  disponible: boolean;
+}
+
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
@@ -8,6 +13,27 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  fechaSeleccionada: string = new Date().toISOString();
 
+  horarios: Horario[] = [
+    { hora: '08:00', disponible: true },
+    { hora: '09:00', disponible: false },
+    { hora: '10:00', disponible: true },
+    { hora: '11:00', disponible: true },
+    { hora: '12:00', disponible: false },
+  ];
+
+  reservarHorario(horario: Horario) {
+    if (horario.disponible) {
+      horario.disponible = false;
+    }
+  }
+
+  get turnosDisponibles(): number {
+    return this.horarios.filter(h => h.disponible).length;
+  }
+
+  onFechaCambiada(event: any) {
+    this.fechaSeleccionada = event.detail.value;
+  }
 }
