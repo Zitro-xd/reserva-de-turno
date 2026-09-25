67927b0b27d9c8aa4cf7a063b05398b0fc187f6b
Davih_xd <elweydelpito1234@gmail.com>
2026-09-24 22:35:29 -0300

    main

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,38 @@
 import { Component } from '@angular/core';
 
+interface Horario {
+  hora: string;
+  disponible: boolean;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
-  standalone: false,
 })
 export class AppComponent {
-  constructor() {}
+
+  fechaSeleccionada: string = new Date().toISOString();
+
+  horarios: Horario[] = [
+    { hora: '08:00', disponible: true },
+    { hora: '09:00', disponible: false },
+    { hora: '10:00', disponible: true },
+    { hora: '11:00', disponible: true },
+    { hora: '12:00', disponible: false },
+  ];
+
+  reservarHorario(horario: Horario) {
+    if (horario.disponible) {
+      horario.disponible = false;
+    }
+  }
+
+  get turnosDisponibles(): number {
+    return this.horarios.filter(h => h.disponible).length;
+  }
+
+  onFechaCambiada(event: any) {
+    this.fechaSeleccionada = event.detail.value;
+  }
 }
